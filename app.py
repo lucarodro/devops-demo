5333edd2427be548b0b9c1d89a042b5c0224c623
lucarodro <lucarodro@gmail.com>
2025-11-16 14:39:57 +0100

    add farewell function

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ def greet(name):
     return f"Hello, {name}!"
 
 
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+
 if __name__ == "__main__":
     print(greet("DevOps"))
--- test_app.py
+++ test_app.py
@@ -4,3 +4,7 @@ from app import greet
 def test_greet():
     assert greet("Alice") == "Hello, Alice!"
     assert greet("Bob") == "Hello, Bob!"
+
+
+def test_farewell():
+    assert farewell("Alice") == "Goodbye, Alice!"
